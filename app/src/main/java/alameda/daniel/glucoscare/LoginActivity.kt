9e43ce0591784b67e3e13acea5ab9a2fd8930433
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2024-05-07 19:23:22 -0700

    implementación registro nivel azucar, login, registrar

--- app/src/main/java/alameda/daniel/glucoscare/LoginActivity.kt
+++ app/src/main/java/alameda/daniel/glucoscare/LoginActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var btn_ingresar: Button
     lateinit var btn_registro: Button
     lateinit var tv_recuperacion: TextView
+    private lateinit var correoEditText: EditText
+    private lateinit var contraseniaEditText: EditText
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -17,21 +25,74 @@ class LoginActivity : AppCompatActivity() {
         btn_ingresar = findViewById(R.id.btn_ingresar)
         btn_registro = findViewById(R.id.btn_registro)
         tv_recuperacion = findViewById(R.id.tv_recuperacion)
+        correoEditText = findViewById(R.id.txt_email_login)
+        contraseniaEditText = findViewById(R.id.txt_contrasenia_login)
+        auth = Firebase.auth
 
-        btn_registro.setOnClickListener{
+        btn_registro.setOnClickListener {
             var intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
         }
 
-        btn_ingresar.setOnClickListener{
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        btn_ingresar.setOnClickListener {
+            login()
         }
 
-        tv_recuperacion.setOnClickListener{
+        tv_recuperacion.setOnClickListener {
             var intent = Intent(this, RecuperacionActivity::class.java)
             startActivity(intent)
         }
 
     }
+
+
+    private fun login() {
+        var correo: String = correoEditText.text.toString()
+        var contra: String = contraseniaEditText.text.toString()
+
+        if (!correo.isNullOrEmpty() && !contra.isNullOrEmpty()) {
+            auth.signInWithEmailAndPassword(correo, contra)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        //Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        val userId = user?.uid
+                        val correoUsuario = user?.email
+
+                        val sharedPreferences = getSharedPreferences("userInfo", MODE_PRIVATE)
+                        val editor = sharedPreferences.edit()
+                        editor.putString("id_usuario", userId)
+                        editor.putString("correo", correoUsuario)
+                        editor.apply()
+
+                        Toast.makeText(
+                            this,
+                            "Bienvenido",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                        var intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        // Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this,
+                            "Datos incorrectos.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        //updateUI(null)
+                    }
+                }
+
+        } else {
+            Toast.makeText(this, "Ingresar datos", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
+}
--- app/src/main/java/alameda/daniel/glucoscare/RegistroActivity.kt
+++ app/src/main/java/alameda/daniel/glucoscare/RegistroActivity.kt
@@ -1,29 +1,173 @@
 package alameda.daniel.glucoscare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegistroActivity : AppCompatActivity() {
     lateinit var genero: Spinner
     lateinit var tipoDiabetes: Spinner
-    lateinit var  adapterGenero: ArrayAdapter<CharSequence>
-    lateinit var  adapterDiabetes: ArrayAdapter<CharSequence>
+    lateinit var adapterGenero: ArrayAdapter<CharSequence>
+    lateinit var adapterDiabetes: ArrayAdapter<CharSequence>
+
+    private lateinit var nombreEditText: EditText
+    private lateinit var edadEditText: EditText
+    private lateinit var telefonoEditText: EditText
+    private lateinit var correoEditText: EditText
+    private lateinit var contraseniaEditText: EditText
+    private lateinit var contraseniaConfEditText: EditText
+    private lateinit var generoSpinner: Spinner
+    private lateinit var tipoDiabetesSpinner: Spinner
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        llenarComboBox();
+        auth = Firebase.auth;
 
+        nombreEditText = findViewById(R.id.txt_nombre)
+        edadEditText = findViewById(R.id.txt_edad)
+        telefonoEditText = findViewById(R.id.txt_telefono)
+        correoEditText = findViewById(R.id.txt_correo)
+        contraseniaEditText = findViewById(R.id.txt_contrasenia)
+        contraseniaConfEditText = findViewById(R.id.txt_contrasenia_conf)
+        generoSpinner = findViewById(R.id.genero)
+        tipoDiabetesSpinner = findViewById(R.id.tipoDiab)
+
+        val btnRegistrar = findViewById<Button>(R.id.btn_registar)
+        btnRegistrar.setOnClickListener {
+            validarUsuario()
+        }
+
+
+    }
+
+
+    private fun llenarComboBox() {
         genero = findViewById(R.id.genero);
-        adapterGenero = ArrayAdapter.createFromResource(this, R.array.genero_items, android.R.layout.simple_spinner_item)
+        adapterGenero = ArrayAdapter.createFromResource(
+            this,
+            R.array.genero_items,
+            android.R.layout.simple_spinner_item
+        )
         adapterGenero.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         genero.setAdapter(adapterGenero)
         genero.setSelection(0)
 
         tipoDiabetes = findViewById(R.id.tipoDiab)
-        adapterDiabetes = ArrayAdapter.createFromResource(this, R.array.daibete_items, android.R.layout.simple_spinner_item)
+        adapterDiabetes = ArrayAdapter.createFromResource(
+            this,
+            R.array.daibete_items,
+            android.R.layout.simple_spinner_item
+        )
         adapterDiabetes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         tipoDiabetes.setAdapter(adapterDiabetes)
         tipoDiabetes.setSelection(0)
+
+    }
+
+
+    private fun validarUsuario() {
+        val nombre = nombreEditText.text.toString().trim()
+        val edadText = edadEditText.text.toString().trim()
+        val telefono = telefonoEditText.text.toString().trim()
+        val correo = correoEditText.text.toString().trim()
+        val contrasenia = contraseniaEditText.text.toString().trim()
+        val contraseniaConf = contraseniaConfEditText.text.toString().trim()
+        val genero = generoSpinner.selectedItem.toString()
+        val tipoDiabetes = tipoDiabetesSpinner.selectedItem.toString()
+
+        if (nombre.isEmpty() || edadText.isEmpty() || telefono.isEmpty() || correo.isEmpty() ||
+            contrasenia.isEmpty() || contraseniaConf.isEmpty() || genero.isEmpty() ||
+            tipoDiabetes.isEmpty()
+        ) {
+            Toast.makeText(
+                this,
+                "Algún campo esta vacio, verifique otra vez",
+                Toast.LENGTH_SHORT,
+            ).show()
+            return
+        }
+
+        // Validar que las contraseñas coincidan
+        if (contrasenia != contraseniaConf) {
+            Toast.makeText(
+                this,
+                "Las contraseñas no coinciden",
+                Toast.LENGTH_SHORT,
+            ).show()
+            return
+        }
+
+        if (contrasenia.length < 6) {
+            Toast.makeText(
+                this,
+                "Las contraseñas debe de ser mayor a 6 caracteres",
+                Toast.LENGTH_SHORT,
+            ).show()
+            return
+        }
+
+        var usuario =
+            Usuario(nombre, edadText.toInt(), telefono, correo, contrasenia, genero, tipoDiabetes)
+        registarUsuario(usuario)
+
+
+    }
+
+
+    public fun registarUsuario(usuario: Usuario) {
+
+        var correo = usuario.correo;
+        var contrasenia = usuario.contrasenia
+        // Crear usuario en Firebase Authentication
+        auth.createUserWithEmailAndPassword(correo, contrasenia)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val userId = auth.currentUser!!.uid
+
+                    val database = FirebaseDatabase.getInstance()
+                    val usersRef = database.getReference("users")
+
+                    usersRef.child(userId).setValue(usuario)
+                        .addOnSuccessListener {
+                            Log.d("exito", "createUserWithEmail:success")
+                            Toast.makeText(
+                                this,
+                                "Se ha registrado con exito usuario",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        .addOnFailureListener {
+                            Log.w("error", "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                this,
+                                "No se pudo registrar el usuario",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                } else {
+                    Log.w("error", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this,
+                        "No se pudo Autenticar",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
     }
 }
